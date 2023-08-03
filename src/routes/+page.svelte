b45edd24cadbe0d9558e653b2bbd28820d307030
Surya Pratap Singh Shekhawat <suryashekhawat@users.noreply.github.com>
2023-08-03 09:28:30 +0530

    update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,22 +1,38 @@
 <script>
-    import NavBar from './nav.svelte';
+	import NavBar from './nav.svelte';
+    import MainBannerMobileImg from '$lib/images/main-banner-mobile.svg'
 </script>
 
-
 <div id="main-top">
-    <NavBar />
-    
-    <NavBar />
-    
+	<NavBar navId={'main-nav'} isLogoVisible={true} isSignupButtonVisible={true} />
+	<div style="display: grid; justify-content: center;">
+		<NavBar
+			navId={'main-nav-secondary'}
+			bgColor={'#FFFFFF'}
+			isLogoVisible={false}
+			isSignupButtonVisible={false}
+		/>
+	</div>
+    <img id="main-banner-mobile-app-img" src={MainBannerMobileImg} alt="main-app-demo">
 </div>
 
-
 <style>
+	#main-top {
+		background: rgb(238, 200, 239);
+		background: linear-gradient(
+			243deg,
+			rgba(238, 200, 239, 1) 0%,
+			rgba(241, 235, 222, 1) 35%,
+			rgba(225, 216, 243, 1) 100%
+		);
+		height: 726px;
+        overflow: hidden;
+        position: relative;
+	}
 
-    #main-top {
-        background: rgb(238,200,239);
-        background: linear-gradient(243deg, rgba(238,200,239,1) 0%, rgba(241,235,222,1) 35%, rgba(225,216,243,1) 100%);
-        height: 726px;
+    #main-banner-mobile-app-img {
+        position: absolute;
+        right: 11.444444%;
     }
-
+    
 </style>
--- src/routes/nav.svelte
+++ src/routes/nav.svelte
@@ -1,59 +1,104 @@
 <script>
-    import LogoImg from '$lib/images/logo.svg';
-    import Button from './button.svelte';
-    export const bgColor = 'transparent';
-    const navLinks = [
-        {
-            name: 'Product',
-            url: '/#',
-            icon: ''
-        },
-        {
-            name: 'About Us',
-            url: '/#',
-            icon: ''
-        },
-        {
-            name: 'Careers',
-            url: '/#',
-            icon: ''
-        },
-        {
-            name: 'Gifts',
-            url: '/#',
-            icon: ''
-        },
-        {
-            name: 'FAQs',
-            url: '/#',
-            icon: ''
-        },
-    ];
+	import homeIcon from '$lib/icons/home.svg';
+	import LogoImg from '$lib/images/logo.svg';
+	import Button from './button.svelte';
+	export let bgColor = 'transparent';
+	export let isLogoVisible = false;
+	export let isSignupButtonVisible = false;
+	export let navId = 'main-nav';
+	const navLinks = [
+		{
+			name: 'Product',
+			url: '/#',
+			icon: ''
+		},
+		{
+			name: 'About Us',
+			url: '/#',
+			icon: ''
+		},
+		{
+			name: 'Careers',
+			url: '/#',
+			icon: ''
+		},
+		{
+			name: 'Gifts',
+			url: '/#',
+			icon: ''
+		},
+		{
+			name: 'FAQs',
+			url: '/#',
+			icon: ''
+		}
+	];
 </script>
-<nav>
-    <div id="main-nav" style="background-color: {bgColor}">
-        <div class="logo">
-            <img src={LogoImg} alt="" srcset="">
-        </div>
-        <div class="nav-links">
-            {#each navLinks as { name, url }}
-                <a href={url}>{name}</a>
-            {/each}
-        </div>
-        <div class="cta">
-            <Button>Sign up</Button>
-        </div>
-    </div>
-</nav>
+
+<div id={navId + '-container'}>
+	<div id={navId} style="background-color: {bgColor}">
+		{#if isLogoVisible}
+			<div class="logo">
+				<img src={LogoImg} alt="" srcset="" />
+			</div>
+		{/if}
+
+		<div class="nav-links">
+			<img class="nav-link-icon" src={homeIcon} alt="home icon" />
+			{#each navLinks as { name, url }}
+				<a href={url}>{name}</a>
+			{/each}
+		</div>
+		{#if isSignupButtonVisible}
+			<div class="cta">
+				<Button>Sign up</Button>
+			</div>
+		{/if}
+	</div>
+</div>
 
 <style>
-    #main-nav {
-        display: grid;
-        grid-template-columns: 1fr 5fr 1fr;
+	#main-nav-secondary-container {
+		width: 992px;
+	}
+	#main-nav {
+		display: grid;
+		grid-template-columns: 1fr 5fr 1fr;
+		padding: 20px;
+		padding-left: 5%;
+		padding-right: 5%;
+		width: 100%;
+	}
+
+	#main-nav-secondary {
+		display: grid;
+		grid-template-columns: 1fr;
+		border-radius: 16px;
+		max-width: 944px;
+		max-height: 56px;
+		padding: 7px 30px 7px 30px;
+		border-color: #f8f8fb;
+		border-width: 1px;
+		border-style: solid;
+		width: 100%;
+	}
+
+	.nav-links {
+		display: flex;
+		flex-direction: row;
+        justify-content: space-between;
+	}
+    .nav-link-icon {
+        width: 24px;
+        height: 24px;
     }
 
-    .logo {
-        width: 114.65px;
-        height: 40px;
-    }
+	.logo {
+		width: 114.65px;
+		height: 40px;
+	}
+	a {
+		text-decoration: none;
+		color: #222222;
+	}
 </style>
