695b879c4571be7d580a70fadd3385d00ca78ff8
Surya Pratap Singh Shekhawat <suryashekhawat@users.noreply.github.com>
2023-08-05 17:53:36 +0530

    raitings

--- src/routes/raiting.svelte
+++ src/routes/raiting.svelte
@@ -6,10 +6,45 @@
 	import AppStore from '$lib/images/app-store.svg';
 	import PlayStore from '$lib/images/play-store.svg';
 
-    import AppleStore from '$lib/icons/apple-store.svg';
-    import GoogleStore from '$lib/icons/google-play.svg';
+	import AppleStore from '$lib/icons/apple-store.svg';
+	import GoogleStore from '$lib/icons/google-play.svg';
 </script>
 
+<section id="story-sandook">
+	<div>
+		<div class="boxes">
+			<span />
+			<span />
+			<span />
+		</div>
+		<div>
+			<h1>the story of Sandook begins with Trust.</h1>
+		</div>
+	</div>
+	<div>
+		<p>
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+			labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+			laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+			voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+			non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+		</p>
+		<p>
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+			labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+			laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+			voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+			non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+		</p>
+		<p>
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+			labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+			laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+			voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+			non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+		</p>
+	</div>
+</section>
 <section>
 	<div id="rating-section">
 		<div class="platfroms">
@@ -100,11 +135,11 @@
 		margin-top: 50px;
 		text-align: center;
 		width: 80%;
-        font-size: 1.5em;
+		font-size: 1.5em;
 	}
 	.store a {
-        color: #000;
-        text-decoration: none;
+		color: #000;
+		text-decoration: none;
 		background-color: white;
 		display: flex;
 		padding: 16px 50px;
@@ -115,5 +150,55 @@
 		border-radius: 45px;
 		width: 219px;
 		height: 36px;
+	}
+	#story-sandook {
+		background: #ffb317;
+		display: flex;
+		flex-direction: row;
+		justify-content: flex-start;
+		align-items: flex-start;
+		padding: 10%;
+		min-height: 384px;
+	}
+
+	#story-sandook .boxes {
+		display: flex;
+		flex-direction: row;
+		margin-bottom: 100px;
+	}
+	.boxes span {
+		width: 125px;
+		height: 125px;
+		border-radius: 16px;
+		background: rgba(0, 0, 0, 0.1);
+		margin-right: 23px;
+	}
+	#story-sandook h1 {
+		color: rgba(0, 0, 0, 0.6);
+		font-family: CircularStdFont;
+		font-size: 40px;
+		font-style: normal;
+		font-weight: 700;
+		line-height: 114%; /* 45.6px */
+		letter-spacing: -0.8px;
+	}
+    #story-sandook p {
+		margin-top: 0;
+		font-family: CircularStdFont;
+		color: rgba(0, 0, 0, 0.6);
+		font-size: 16px;
+		font-style: normal;
+		font-weight: 450;
+		line-height: 24px; /* 150% */
+	}
+
+	#story-sandook p:nth-child(1) {
+		margin-top: 0;
+		font-family: CircularStdFont;
+		color: rgba(0, 0, 0, 0.6);
+		font-size: 16px;
+		font-style: normal;
+		font-weight: 450;
+		line-height: 24px; /* 150% */
 	}
 </style>
