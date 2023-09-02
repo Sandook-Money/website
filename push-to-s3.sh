9268d33afd24737d91fd7921c6cbd02ac16253e0
Surya Pratap Singh Shekhawat <suryashekhawat@users.noreply.github.com>
2023-09-02 20:10:15 +0530

    update

--- push-to-s3.sh
+++ push-to-s3.sh
@@ -1,1 +1,8 @@
-aws s3 cp build/ s3://sandook.money --recursive --profile sandook
+aws s3 cp website/ s3://sandook.money --recursive --profile sandook
+
+
+aws s3 cp chartjs/ s3://calculator-sandook-app --resursive --profile sandook
+
+aws cloudfront create-invalidation --distribution-id E2GH7VZCW8MDM6 --paths "/*" --profile sandook # Website
+
+aws cloudfront create-invalidation --distribution-id E6HAFLM2ID2V0 --paths "/*" --profile sandook # Website
