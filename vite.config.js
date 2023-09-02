91e3bd99627625a145cfbb5bc617933190946caf
Surya Pratap Singh Shekhawat <suryashekhawat@users.noreply.github.com>
2023-09-02 20:12:15 +0530

    removed svelte website

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
-import sveltePreprocess from 'svelte-preprocess';
-export default defineConfig({
-	plugins: [sveltekit({
-		preprocess: sveltePreprocess(),
-	})]
-});
